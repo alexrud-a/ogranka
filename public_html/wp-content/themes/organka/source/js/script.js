93a7d2f37e3995b24e61d9d086d3ac387cc55c14
AleXandra Rudivickaya <rag@one-touch.ru>
2020-08-14 03:45:50 +0300

    header

--- public_html/wp-content/themes/organka/source/js/script.js
+++ public_html/wp-content/themes/organka/source/js/script.js
@@ -2,7 +2,7 @@ var $ = jQuery;
 
 jQuery(document).ready(function($){
     $('.js-toogle').on('click', function () {
-        $(this).toggle('nav__btn-toggle--open');
-        $('.nav__menu-wrap').toggle('nav__menu-wrap--open')
+        $(this).toggleClass('nav__btn-toggle--open');
+        $('.nav__menu-wrap').toggleClass('nav__menu-wrap--open')
     });
 });
