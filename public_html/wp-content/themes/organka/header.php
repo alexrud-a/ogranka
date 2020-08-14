89f0c2f616b17ce55af76f9110dc6718e2ccdc00
AleXandra Rudivickaya <rag@one-touch.ru>
2020-08-14 03:42:15 +0300

    header

--- public_html/wp-content/themes/organka/header.php
+++ public_html/wp-content/themes/organka/header.php
@@ -26,7 +26,7 @@
             <div class="row">
                 <div class="col-xs-12">
                     <div class="nav">
-                        <button class="nav__btn-toogle js-toogle">
+                        <button class="nav__btn-toggle js-toggle">
                             <span></span>
                             <span></span>
                             <span></span>
@@ -52,7 +52,7 @@
                 </div>
                 <div class="header__cart">
                     <a href="#">
-                        <svg width="20" viewBox="0 0 511.81 511.81">
+                        <svg width="30" viewBox="0 0 511.81 511.81">
                             <path d="M477.51 491.42L451.03 142.6a16 16 0 0 0-15.95-14.78H76.39a16 16 0 0 0-15.95 14.8l-26.7 352a16.01 16.01 0 0 0 15.95 17.2h412.43a16 16 0 0 0 15.4-20.39zM66.94 479.81l24.28-320h329.03l24.29 320H66.94z"/><path d="M255.74 0c-70.58 0-128 57.44-128 128.03v63.78a16 16 0 0 0 32 0v-63.78c0-52.96 43.05-96.03 96-96.03 52.94 0 96 43.07 96 96.03v63.78a16 16 0 0 0 32 0v-63.78C383.74 57.44 326.3 0 255.74 0z"/>
                         </svg>
                     </a>
--- public_html/wp-content/themes/organka/source/js/script.js
+++ public_html/wp-content/themes/organka/source/js/script.js
@@ -2,7 +2,7 @@ var $ = jQuery;
 
 jQuery(document).ready(function($){
     $('.js-toogle').on('click', function () {
-        $(this).toggle('nav__btn-toogle--open');
-        $(this).closest('.nav__menu-wrap').toggle('nav__menu-wrap--open')
+        $(this).toggle('nav__btn-toggle--open');
+        $('.nav__menu-wrap').toggle('nav__menu-wrap--open')
     });
 });
